9cd347d7e2fa9393669c5409ef3aab6350ec3bbf
grantcolley <grantcolley@yahoo.com>
2015-10-04 21:49:42 +0100

    WIP
    WIP

--- DevelopmentInProgress.WPFControls/FilterTree/XamlFilterTree.cs
+++ DevelopmentInProgress.WPFControls/FilterTree/XamlFilterTree.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using DevelopmentInProgress.WPFControls.Command;
 
 namespace DevelopmentInProgress.WPFControls.FilterTree
 {
     public class XamlFilterTree : Control
     {
+        private ICommand itemDeletedCommand;
+
         private readonly static DependencyProperty HeaderProperty;
         private readonly static DependencyProperty FilterTextProperty;
         private readonly static DependencyProperty ItemsSourceProperty;
         private readonly static DependencyProperty IsExpandedProperty;
-
+        private static readonly RoutedEvent ItemDeletedEvent;
+            
         static XamlFilterTree()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof (XamlFilterTree),
@@ -25,11 +30,15 @@ namespace DevelopmentInProgress.WPFControls.FilterTree
 
             ItemsSourceProperty = DependencyProperty.Register("ItemsSource", typeof(IEnumerable),
                 typeof(XamlFilterTree), new FrameworkPropertyMetadata(new List<object>()));
+
+            ItemDeletedEvent = EventManager.RegisterRoutedEvent("ItemDeleted", RoutingStrategy.Bubble,
+                typeof (RoutedEventHandler), typeof (XamlFilterTree));
         }
 
         public XamlFilterTree()
         {
             ItemsSource = new List<object>();
+            itemDeletedCommand = new WpfCommand(OnItemDeleted);
         }
 
         public string Header
@@ -54,6 +63,29 @@ namespace DevelopmentInProgress.WPFControls.FilterTree
         {
             get { return (List<object>)GetValue(ItemsSourceProperty); }
             set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public ICommand ItemDeletedCommand
+        {
+            get { return itemDeletedCommand; }
+            set { itemDeletedCommand = value; }
+        }
+
+        public event RoutedEventHandler ItemDeleted
+        {
+            add { AddHandler(ItemDeletedEvent, value); }
+            remove { RemoveHandler(ItemDeletedEvent, value); }
+        }
+
+        private void OnItemDeleted(object arg)
+        {
+            if (arg == null)
+            {
+                return;
+            }
+
+            var args = new RoutedEventArgs(ItemDeletedEvent, arg);
+            RaiseEvent(args);
         }
 
         private void TextBoxBaseOnTextChanged(object sender, TextChangedEventArgs e)
