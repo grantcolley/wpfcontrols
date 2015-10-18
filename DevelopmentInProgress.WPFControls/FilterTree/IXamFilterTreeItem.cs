1b7c3b7e558b722e5222561743589a03792edaa6
grantcolley <grantcolley@yahoo.com>
2015-10-18 21:20:27 +0100

    Rename IXamFilterTree to IXamlFilterTree
    Rename IXamFilterTree to IXamlFilterTree

--- DevelopmentInProgress.WPFControls/FilterTree/IXamFilterTreeItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevelopmentInProgress.WPFControls.FilterTree
-{
-    public interface IXamFilterTreeItem
-    {
-        string Text { get; set; }
-        bool IsVisible { get; set; }
-    }
-}
