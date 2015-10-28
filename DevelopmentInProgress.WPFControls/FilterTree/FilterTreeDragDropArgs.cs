742ae4f2fd0f52aa2a0d37519940b9c18783ca76
grantcolley <grantcolley@yahoo.com>
2015-10-28 22:34:57 +0000

    Add FilterTreeDragDropArgs.cs
    Add FilterTreeDragDropArgs.cs

---
+++ DevelopmentInProgress.WPFControls/FilterTree/FilterTreeDragDropArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace DevelopmentInProgress.WPFControls.FilterTree
+{
+    public class FilterTreeDragDropArgs
+    {
+        public FilterTreeDragDropArgs(object dragItem, object dropTarget)
+        {
+            DragItem = dragItem;
+            DropTarget = dropTarget;
+        }
+
+        public object DragItem { get; private set; }
+        public object DropTarget { get; private set; }
+    }
+}
