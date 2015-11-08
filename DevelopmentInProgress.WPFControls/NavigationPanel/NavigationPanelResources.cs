3cb86d2226222d77d671ce48bd85f8668a1d9299
grantcolley <grantcolley@yahoo.com>
2015-11-08 22:09:34 +0000

    Add code behind for the Navigation Panel resource file
    Add code behind for the Navigation Panel resource file

---
+++ DevelopmentInProgress.WPFControls/NavigationPanel/NavigationPanelResources.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DevelopmentInProgress.WPFControls.NavigationPanel
+{
+    partial class NavigationPanelResources
+    {
+        private void ExpanderImageMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            throw new System.NotImplementedException();
+
+                                    //            <!--<i:Interaction.Triggers>
+                                    //    <i:EventTrigger EventName="MouseDown">
+                                    //        <i:InvokeCommandAction Command="{Binding ExpanderChangedCommand, RelativeSource={RelativeSource TemplatedParent}}"
+                                    //                               CommandParameter="{Binding SelectedItem, ElementName=modulesList}"/>
+                                    //    </i:EventTrigger>
+                                    //</i:Interaction.Triggers>-->
+
+
+        }
+
+        private void ModulesListOnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+                                //            <!--<i:Interaction.Triggers>
+                                //    <i:EventTrigger EventName="SelectionChanged">
+                                //        <i:InvokeCommandAction Command="{Binding SelectionChangedCommand, RelativeSource={RelativeSource TemplatedParent}}"
+                                //                               CommandParameter="{Binding SelectedItem, ElementName=modulesList}"/>
+                                //    </i:EventTrigger>
+                                //</i:Interaction.Triggers>-->
+        }
+    }
+}
