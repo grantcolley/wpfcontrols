fe738c3c56cf51da8f6b38a2ea07db9b4e272cd7
grantcolley <grantcolley@yahoo.com>
2015-11-12 12:53:15 +0000

    Add comment
    Add comment

--- DevelopmentInProgress.WPFControls/NavigationPanel/NavigationPanelResources.cs
+++ DevelopmentInProgress.WPFControls/NavigationPanel/NavigationPanelResources.cs
@@ -1,8 +1,18 @@
-﻿using System.Windows.Controls;
+﻿//-----------------------------------------------------------------------
+// <copyright file="NavigationPanelResources.cs" company="Development In Progress Ltd">
+//     Copyright © 2015. All rights reserved.
+// </copyright>
+// <author>Grant Colley</author>
+//-----------------------------------------------------------------------
+
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace DevelopmentInProgress.WPFControls.NavigationPanel
 {
+    /// <summary>
+    /// The <see cref="NavigationPanelResources"/> class provides the code behind the resource dictionary. 
+    /// </summary>
     partial class NavigationPanelResources
     {
         private void ExpanderImageMouseDown(object sender, MouseButtonEventArgs e)
