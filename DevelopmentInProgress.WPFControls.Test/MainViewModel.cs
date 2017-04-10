00ac97588f28fd38f8115f15d2f2191059381b9b
grantcolley <grantcolley@yahoo.com>
2017-04-10 13:22:12 +0100

    Add DevelopmentInProgress.WPFControls.Test project.
    Add DevelopmentInProgress.WPFControls.Test project.

---
+++ DevelopmentInProgress.WPFControls.Test/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿namespace DevelopmentInProgress.WPFControls.Test
+{
+    public class MainViewModel
+    {
+
+
+
+            //var moduleSettings = new ModuleSettings();
+            //moduleSettings.ModuleName = ModuleName;
+            //moduleSettings.ModuleImagePath = @"/DevelopmentInProgress.ControlsExample;component/Images/User_Manage.png";
+
+            //var usersGroup = new ModuleGroup();
+            //usersGroup.ModuleGroupName = "Users";
+
+            //var userList = new ModuleGroupItem();
+            //userList.ModuleGroupItemName = "User List";
+            //userList.TargetView = typeof(UserListView).Name;
+            //userList.TargetViewTitle = "User List";
+            //userList.ModuleGroupItemImagePath = @"/DevelopmentInProgress.ControlsExample;component/Images/User_List.png";
+
+            //var userAdd = new ModuleGroupItem();
+            //userAdd.ModuleGroupItemName = "Add User";
+            //userAdd.TargetView = typeof(UserListView).Name;
+            //userAdd.TargetViewTitle = "User Add";
+            //userAdd.ModuleGroupItemImagePath = @"/DevelopmentInProgress.ControlsExample;component/Images/User_Add.png";
+
+            //var rolesAndActivitiesGroup = new ModuleGroup();
+            //rolesAndActivitiesGroup.ModuleGroupName = "Roles & Activities";
+
+            //var roles = new ModuleGroupItem();
+            //roles.ModuleGroupItemName = "Roles";
+            //roles.TargetView = typeof(UserListView).Name;
+            //roles.TargetViewTitle = "Roles";
+            //roles.ModuleGroupItemImagePath = @"/DevelopmentInProgress.ControlsExample;component/Images/Roles.png";
+
+            //var activities = new ModuleGroupItem();
+            //activities.ModuleGroupItemName = "Activities";
+            //activities.TargetView = typeof(UserListView).Name;
+            //activities.TargetViewTitle = "Activities";
+            //activities.ModuleGroupItemImagePath = @"/DevelopmentInProgress.ControlsExample;component/Images/Activities.png";
+
+            //usersGroup.ModuleGroupItems.Add(userList);
+            //usersGroup.ModuleGroupItems.Add(userAdd);
+            //moduleSettings.ModuleGroups.Add(usersGroup);
+
+            //rolesAndActivitiesGroup.ModuleGroupItems.Add(roles);
+            //rolesAndActivitiesGroup.ModuleGroupItems.Add(activities);
+            //moduleSettings.ModuleGroups.Add(rolesAndActivitiesGroup);
+
+            //ModuleNavigator.AddModuleNavigation(moduleSettings);
+
+
+    }
+}
---
+++ DevelopmentInProgress.WPFControls.Test/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace DevelopmentInProgress.WPFControls.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+        }
+    }
+}
