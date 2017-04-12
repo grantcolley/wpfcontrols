81c534251ea4d4d6313b3e2f45d5e040e8598874
grantcolley <grantcolley@yahoo.com>
2017-04-12 09:10:50 +0100

    Add navigation panel and messages.
    Add navigation panel and messages.

--- DevelopmentInProgress.WPFControls.Test/MainViewModel.cs
+++ DevelopmentInProgress.WPFControls.Test/MainViewModel.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Input;
-using DevelopmentInProgress.WPFControls.NavigationPanel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using DevelopmentInProgress.WPFControls.Messaging;
 
 namespace DevelopmentInProgress.WPFControls.Test
 {
@@ -10,6 +9,32 @@ namespace DevelopmentInProgress.WPFControls.Test
 
         public MainViewModel()
         {
+            LoadMessages();
+        }
+
+        public ObservableCollection<Message> Messages { get; set; }
+
+        private void LoadMessages()
+        {
+            var messageError = new Message()
+            {
+                MessageType = MessageType.Error,
+                Text = "The role name is mandatory when creating or saving a role."
+            };
+
+            var messageWarn = new Message()
+            {
+                MessageType = MessageType.Warn,
+                Text = "Cannot create a new role with the name 'Writer' as one already exists."
+            };
+
+            var messageInfo = new Message()
+            {
+                MessageType = MessageType.Info,
+                Text = "User 'Joe Bloggs' has been granted the role 'Writer'."
+            };
+
+            Messages = new ObservableCollection<Message>(new List<Message> { messageError, messageWarn, messageInfo });
         }
     }
 }
